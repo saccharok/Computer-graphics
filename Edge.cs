a7cfb43454acf3ba437499fbf990202e4007ed2f
saccharok <90149992+saccharok@users.noreply.github.com>
2024-01-23 23:14:21 +0700

    warnok

---
+++ Edge.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cg_lr2
+{
+    internal class Edge
+    {
+        private Point3D p1;
+        private Point3D p2;
+
+        public Point3D P1 => p1;
+        public Point3D P2 => p2;
+
+        public Edge(Point3D p1, Point3D p2)
+        {
+            this.p1 = p1;
+            this.p2 = p2;
+        }
+
+        public Edge(Edge edge)
+        {
+            this.p1 = edge.P1;
+            this.p2 = edge.P2;
+        }
+    }
+}
